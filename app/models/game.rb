dfa875097d78ce522481f1a95950d8ccd0b5b269
Moyoalao <moyosore921@gmail.com>
2023-12-17 21:19:28 +0000

    final

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,8 @@
 
 # game
 class Game < ApplicationRecord
-  validates :status, inclusion: { in: %w[playing played will_play],
-                                  message: '%<value>s is not a valid status' }
-
+  #this ensures that the values possible for status are the ones specified 
+  validates :status, inclusion: { in: %w[playing played will_play], message: '%<value>s is not a valid status' }
+  #makes sure ours played is inputed unless the ststus is will_play
   validates :hours_played, presence: true, unless: -> { status == 'will_play' }
 end
